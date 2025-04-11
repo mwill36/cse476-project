20eca6dec845737ccf3e22e0ef1339f03c02614e
ziad-bakki <bakkizia@msu.edu>
2025-04-11 16:08:19 -0400

    removed camera setting up

--- app/src/main/java/edu/msu/willemi8/project/HomeActivity.java
+++ app/src/main/java/edu/msu/willemi8/project/HomeActivity.java
@@ -2,29 +2,39 @@ package edu.msu.willemi8.project;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private Button openCameraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        openCameraBtn = findViewById(R.id.openCameraBtn);
 
-        openCameraBtn.setOnClickListener(v -> {
-            // Launch ScannerActivity
-            Intent intent = new Intent(HomeActivity.this, ScannerActivity.class);
-            startActivity(intent);
-        });
     }
+
+
+
+
 
 
     protected boolean onAddItemManually(int id, String name, String expirationDate){
